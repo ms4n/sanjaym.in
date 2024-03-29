33c2b82a705448e776a97acbbc2dedf8d5e01b2a
sanjay <arjunsanjay0@gmail.com>
2024-03-30 03:21:12 +0530

    added horizontal scrolling to cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,24 @@ import Card from "@/components/card";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <div className="flex flex-col h-screen">
         <p className="text-3xl mx-10 mt-10">Projects(4)</p>
-        <div className="flex m-5 gap-5 items-center justify-center text-4xl">
-          <Card
-            src="/images/parrot.jpeg"
-            projectName="High Performant Chat Server"
-            projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
-            badges={["Backend", "WebSockets"]}
-          />
-          <Card
-            src="/images/chat.jpeg"
-            projectName="Chat UI"
-            projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
-            badges={["Frontend", "NextJS"]}
-          />
+        <div className="flex items-center justify-start text-4xl overflow-x-auto">
+          <div className="flex flex-shrink-0 m-5 gap-5">
+            <Card
+              src="/images/parrot.jpeg"
+              projectName="High Performant Chat Server"
+              projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
+              badges={["Backend", "WebSockets"]}
+            />
+            <Card
+              src="/images/chat.jpeg"
+              projectName="Chat UI"
+              projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
+              badges={["Frontend", "NextJS"]}
+            />
+          </div>
         </div>
       </div>
     </div>
