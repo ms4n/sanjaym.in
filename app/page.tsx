c34ad8662d3fcf014ee4822f2692cf24edd0a8cc
sanjay <arjunsanjay0@gmail.com>
2024-03-30 03:09:20 +0530

    created custom card component for projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import React from "react";
+import Card from "@/components/card";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-4xl">
-      Home
+    <div>
+      <div className="flex flex-col h-screen">
+        <p className="text-3xl mx-10 mt-10">Projects(4)</p>
+        <div className="flex m-5 gap-5 items-center justify-center text-4xl">
+          <Card
+            src="/images/parrot.jpeg"
+            projectName="High Performant Chat Server"
+            projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
+            badges={["Backend", "WebSockets"]}
+          />
+          <Card
+            src="/images/chat.jpeg"
+            projectName="Chat UI"
+            projectDescription="Some information about the project, this project is written in Golang, with keeping sheer performance in mind."
+            badges={["Frontend", "NextJS"]}
+          />
+        </div>
+      </div>
     </div>
   );
 };
