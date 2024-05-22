43b9fa64ad9b91104f27a289dc9faac7f7cae141
sanjay <arjunsanjay0@gmail.com>
2024-05-22 18:37:07 +0530

    updated font size for desktop

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import {
   SquareArrowOutUpRight,
   Copy,
   CheckCheck,
   ArrowRight,
+  ChevronDown,
   ArrowDown,
 } from "lucide-react";
 
@@ -17,12 +18,15 @@ import Particles from "./components/particles";
 import Card from "./components/card";
 
 const links = [
-  { text: "github", href: "www.google.com" },
+  { text: "github", href: "https://github.com/ms4n" },
   { text: "linkedin", href: "www.google.com" },
   { text: "resume", href: "www.google.com" },
 ];
 
 export default function Home() {
+  const techStackRef = useRef<HTMLDivElement>(null);
+
+  const [showTechStack, setShowTechStack] = useState(false);
   const [copyStatus, setCopyStatus] = useState(false);
 
   const [gradientColor, setGradientColor] = useState("bg-blue-600");
@@ -65,7 +69,7 @@ export default function Home() {
 
   return (
     <div
-      className={`flex flex-col main h-screen w-full justify-center md:pt-32 overflow-hidden bg-gradient-to-tl from-black ${gradient} to-black`}
+      className={`flex flex-col main h-full w-full justify-center md:pt-32 overflow-hidden bg-gradient-to-tl from-black ${gradient} to-black`}
     >
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
@@ -76,8 +80,10 @@ export default function Home() {
         <div className="px-10 max-w-3xl">
           <nav className="flex items-center justify-between animate-fade-i">
             <div>
-              <p className="text-lg font-playfair text-zinc-200">Sanjay M</p>
-              <p className="text-xs font-satoshim leading-none tracking-tight text-zinc-500">
+              <p className="text-lg md:text-2xl font-playfair text-zinc-200">
+                Sanjay M
+              </p>
+              <p className="text-xs md:text-sm font-satoshim leading-none tracking-tight text-zinc-500">
                 Bengaluru, India
               </p>
             </div>
@@ -100,15 +106,51 @@ export default function Home() {
             </div>
           </nav>
 
-          <div className="my-10 md:max-w-lg">
-            <h2 className="text-sm font-satoshim text-zinc-400">
-              Hi,{" "}
-              <span className="text-zinc-200">
-                I&apos;m Sanjay, a backend-focused fullstack developer{" "}
-              </span>{" "}
-              with a flair for design. I partner with ambitious teams to bring
-              their visions to life.
-              <p className="mt-4">
+          <div className="mt-8 md:max-w-lg md:h-5/6 md:flex md:flex-col md:justify-between">
+            <div>
+              <h2 className="text-sm md:text-base font-satoshim text-zinc-400">
+                Hi,{" "}
+                <span className="text-zinc-200">
+                  I&apos;m Sanjay, a backend-focused fullstack developer{" "}
+                </span>{" "}
+                with a flair for design. I partner with ambitious teams to bring
+                their visions to life.
+              </h2>
+              <div className="mt-2 block md:hidden text-sm font-satoshim text-zinc-400">
+                Although I&apos;m unbounded by frameworks or languages, this is{" "}
+                <button
+                  className="text-zinc-200 underline"
+                  onClick={() => setShowTechStack(!showTechStack)}
+                >
+                  <span className="flex items-center">
+                    my go-to stack for building{" "}
+                    <ChevronDown
+                      className="inline-block ml-1 mt-1 transition-transform duration-300 ease-in-out"
+                      style={{
+                        transform: showTechStack ? "rotate(180deg)" : "none",
+                      }}
+                      size={14}
+                    />
+                  </span>
+                </button>
+                <div
+                  ref={techStackRef}
+                  className="text-zinc-200 overflow-hidden transition-all duration-300 ease-in-out"
+                  style={{
+                    maxHeight: showTechStack
+                      ? `${techStackRef.current?.scrollHeight}px` // Optional chaining for safety
+                      : "0",
+                    opacity: showTechStack ? 1 : 0,
+                  }}
+                >
+                  <div className="mt-2">
+                    Python with Django/FastAPI and Node.js with
+                    TypeScript/Express.js/React/Next.js, with a growing interest
+                    in Golang.
+                  </div>
+                </div>
+              </div>
+              <p className="mt-2 md:mt-7 hidden md:block text-sm md:text-base font-satoshim text-zinc-400">
                 Although I&apos;m unbounded by frameworks or languages, my go-to
                 stack for building is{" "}
                 <span className="text-zinc-200">
@@ -117,24 +159,35 @@ export default function Home() {
                 </span>{" "}
                 with a growing interest in Golang.
               </p>
-            </h2>
+            </div>
 
-            <div className="flex items-center gap-4 mt-5">
+            <div className="flex items-center gap-4 my-6">
               <Image
-                className=" rounded-full"
-                src={"/images/sanjay_m.png"}
+                className="rounded-full block md:hidden"
+                src="/images/sanjay_m.png"
                 width={60}
                 height={60}
+                quality={100}
                 alt="sanjay-m circle image"
-              ></Image>
+              />
 
-              <div className="flex flex-col gap-3 text-zinc-400 text-sm font-satoshim">
+              <Image
+                className="rounded-full hidden md:block"
+                src="/images/sanjay_m.png"
+                width={80}
+                height={80}
+                quality={100}
+                alt="sanjay-m circle image"
+              />
+
+              <div className="flex flex-col gap-3 text-sm md:text-base font-satoshim text-zinc-400 font-satoshim">
                 <div className="flex gap-4 underline">
                   {links.map((link, index) => (
                     <Link
                       className="flex items-center gap-2"
                       key={index}
                       href={link.href}
+                      target="_blank"
                     >
                       {link.text} <SquareArrowOutUpRight size={14} />
                     </Link>
@@ -160,7 +213,7 @@ export default function Home() {
 
         <div className="flex flex-col md:max-h-[calc(100vh-40px)] overflow-hidden md:mr-10">
           <h1 className="flex justify-between px-10 md:px-0 text-zinc-200 text-base font-satoshim mb-3">
-            Projects{" "}
+            Projects
             <ArrowRight className="mx-1 mt-2 block md:hidden" size={16} />
             {/* <ArrowDown className="mx-1 mt-2 hidden md:block" size={16} /> */}
           </h1>
